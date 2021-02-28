77dfedc0b9af8c7f660e33a54b9454a8dd96d552
Nikita Tomilov <programmer174@icloud.com>
2021-02-28 21:17:56 +0300

    framework: better handling for beforeAll/afterAll

--- framework/src/main/kotlin/com/nikitatomilov/BenchmarkableTestBase.kt
+++ framework/src/main/kotlin/com/nikitatomilov/BenchmarkableTestBase.kt
@@ -2,22 +2,14 @@ package com.nikitatomilov
 
 import com.nikitatomilov.annotations.BenchmarkableTest
 import com.nikitatomilov.api.TestableSubject
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.DynamicContainer
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
 
 open class BenchmarkableTestBase(
   private val testInstance: Class<*>,
   private val targets: Iterable<TestableSubject>
 ) {
-
-  @BeforeEach
-  fun runBeforeAlls() {
-    targets.forEach { it.beforeAll() }
-  }
-
-  @AfterEach
-  fun runAfterAlls() {
-    targets.forEach { it.afterAll() }
-  }
 
   @TestFactory
   fun benchmarkableTests(): Iterable<DynamicContainer> {
@@ -25,14 +17,17 @@ open class BenchmarkableTestBase(
       it.isAnnotationPresent(BenchmarkableTest::class.java)
     }
     return targets.map { target ->
-      DynamicContainer.dynamicContainer(target::class.java.simpleName, requiredTests.map { testMethod ->
-        DynamicTest.dynamicTest(testMethod.name) {
-          target.beforeEach()
-          testMethod.invoke(this, target)
-          target.afterEach()
-        }
-      })
+      DynamicContainer.dynamicContainer(target::class.java.simpleName,
+          listOf(buildFunc("setUp") { target.beforeAll() } ) +
+          requiredTests.map { testMethod ->
+            DynamicTest.dynamicTest(testMethod.name) {
+              target.beforeEach()
+              testMethod.invoke(this, target)
+              target.afterEach()
+            }
+          }.toList() + listOf(buildFunc("tearDown") { target.afterAll() } ))
     }
   }
 
+  private fun buildFunc(name: String, callback: () -> Unit) = DynamicTest.dynamicTest(name, callback)
 }
